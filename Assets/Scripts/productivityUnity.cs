1d27a3b31d7ca6a1480458c24c3ec126c838cd13
Zion520 <zion_santana@estudante.sesisenai.org.br>
2026-06-15 14:30:44 -0300

    aula 15/06

---
+++ Assets/Scripts/productivityUnity.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class productivityUnity : Unit
+{  
+    //novas variaveis
+    private ResourcePile m_currentPile = null;
+    public float productivityMultiplier = 2;
+
+   protected override void BuildingInRange()
+   {
+    if (m_currentPile == null)
+    {
+        ResourcePile pile 
+    }
+   }
+
+}
